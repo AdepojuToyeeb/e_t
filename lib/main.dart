8543da7b1a85d0aa3f40137d68b2dc2c26c85d5b
Adewale Adepoju <adewale.adepoju14@gmail.com>
2024-09-10 11:53:32 +0100

    added a theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:expense_app/widgets/expenses/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Expenses(),
+    theme: ThemeData().copyWith(
+      scaffoldBackgroundColor: Color.fromARGB(126, 124, 121, 49),
+    ),
+    home: const Expenses(),
   ));
 }
