2791d8a157e01623dd89e183b62eb5524b8a8291
Adewale Adepoju <adewale.adepoju14@gmail.com>
2024-09-10 12:08:24 +0100

    setting and using a color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:expense_app/widgets/expenses/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData().copyWith(
-      scaffoldBackgroundColor: Color.fromARGB(126, 124, 121, 49),
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      // scaffoldBackgroundColor: Color.fromARGB(255, 79, 0, 0),
     ),
     home: const Expenses(),
   ));
